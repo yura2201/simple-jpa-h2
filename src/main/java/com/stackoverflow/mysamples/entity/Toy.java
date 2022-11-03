fa91a094b49e78b97eeba31cf2143c39dae3ea90
Yuriy Tsarkov <yurait6@gmail.com>
2022-11-03 23:53:49 +0300

    added Toy entity

---
+++ src/main/java/com/stackoverflow/mysamples/entity/Toy.java
@@ -0,0 +1,56 @@
+package com.stackoverflow.mysamples.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+/**
+ * @author Yuriy Tsarkov (yurait6@gmail.com) on 03.11.2022
+ */
+@Entity
+@Getter
+@Setter
+@ToString
+@Accessors(chain = true)
+public class Toy {
+  @Id
+  @Column(name = "id", nullable = false)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String madeOf;
+
+  public Long getId() {
+    return id;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+
+    Toy toy = (Toy) o;
+
+    if (getId() != null ? !getId().equals(toy.getId()) : toy.getId() != null)
+      return false;
+    if (getName() != null ? !getName().equals(toy.getName()) : toy.getName() != null)
+      return false;
+    return getMadeOf() != null ? getMadeOf().equals(toy.getMadeOf()) : toy.getMadeOf() == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = getId() != null ? getId().hashCode() : 0;
+    result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+    result = 31 * result + (getMadeOf() != null ? getMadeOf().hashCode() : 0);
+    return result;
+  }
+}
